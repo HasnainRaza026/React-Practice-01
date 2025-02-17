e04f5f9640c83428e284c36d7673969397fa1c2d
HasnainRaza026 <hasnainraza0026@gmail.com>
2025-02-17 09:31:19 +0500

    working on currency-converter practice project

---
+++ currency-converter/src/App.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import Select from "./Select";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="content">
+        <Select />
+      </div>
+    </div>
+  );
+}
+
+export default App;
