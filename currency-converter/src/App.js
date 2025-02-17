6e1a2089e37cca7033ee923ac830bcbbd0d67e3b
HasnainRaza026 <hasnainraza0026@gmail.com>
2025-02-17 12:38:40 +0500

    progress in currency converter project

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,11 +1,60 @@
+import { useState, useEffect } from "react";
 import "./index.css";
-import Select from "./Select";
+import { Select, SelectCurrency, ReverseButton } from "./Select";
+
+const currencyData = [
+  {
+    flag: "./Assets/america.png",
+    symbol: "USD",
+  },
+  {
+    flag: "./Assets/england.png",
+    symbol: "GBP",
+  },
+  {
+    flag: "./Assets/india.png",
+    symbol: "INR",
+  },
+  {
+    flag: "./Assets/pakistan.png",
+    symbol: "PKR",
+  },
+];
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState(currencyData[0]);
+  const [toCurrency, setToCurrency] = useState(currencyData[1]);
+
+  useEffect(() => {
+    if (fromCurrency === toCurrency) {
+      const newToCurrency = currencyData.filter(
+        (elem) => elem !== fromCurrency
+      )[0];
+      setToCurrency(newToCurrency);
+    }
+  }, [fromCurrency, toCurrency]);
+
   return (
     <div className="app">
       <div className="content">
-        <Select />
+        <Select>
+          <SelectCurrency
+            currencyData={currencyData}
+            selectedCurrency={fromCurrency}
+            onSelectCurrency={setFromCurrency}
+          />
+          <ReverseButton
+            fromCurrency={fromCurrency}
+            setFromCurrency={setFromCurrency}
+            toCurrency={toCurrency}
+            setToCurrency={setToCurrency}
+          />
+          <SelectCurrency
+            currencyData={currencyData}
+            selectedCurrency={toCurrency}
+            onSelectCurrency={setToCurrency}
+          />
+        </Select>
       </div>
     </div>
   );
--- currency-converter/src/Select.js
+++ currency-converter/src/Select.js
@@ -1,3 +1,65 @@
-export default function Select() {
-  return <div className="select-main"></div>;
+import { useState } from "react";
+
+export function Select({ children }) {
+  return <div className="select-main">{children}</div>;
+}
+
+export function SelectCurrency({
+  currencyData,
+  selectedCurrency,
+  onSelectCurrency,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnSelectCurrency = (elem) => {
+    onSelectCurrency(elem);
+    setIsOpen(false);
+  };
+  return (
+    <div className="dropdown">
+      <div
+        className="selected-currency"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div>
+          <img src={selectedCurrency.flag} alt="flag" />
+          <p>{selectedCurrency.symbol}</p>
+        </div>
+        <img
+          className="img-expand"
+          src="./Assets/expand_arrow.svg"
+          alt="expand"
+        />
+      </div>
+      {isOpen && (
+        <ul>
+          {currencyData.map((elem) => (
+            <li key={elem.symbol} onClick={() => handleOnSelectCurrency(elem)}>
+              <img src={elem.flag} alt="flag" />
+              <p>{elem.symbol}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export function ReverseButton({
+  fromCurrency,
+  toCurrency,
+  setFromCurrency,
+  setToCurrency,
+}) {
+  const handleOnCurrencySwap = () => {
+    const newFromCurrency = fromCurrency;
+    const newToCurrency = toCurrency;
+    setFromCurrency(newToCurrency);
+    setToCurrency(newFromCurrency);
+  };
+  return (
+    <button onClick={handleOnCurrencySwap}>
+      <img src="./Assets/arrow.svg" alt="button" />
+    </button>
+  );
 }
